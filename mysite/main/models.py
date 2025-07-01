f39dd2d77a1029e9e52635cf100abd0994fd91ae
MarkTru <bambambam228282@gmail.com>
2025-07-02 02:40:55 +0300

    projects model

--- mysite/main/models.py
+++ mysite/main/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Projects(models.Model):
+    title = models.CharField(max_length=100)
+    category = models.CharField(max_length=50)
+    description = models.TextField()
+    image = models.ImageField(upload_to='projects/', blank=True)
+    url = models.URLField(blank=True)
+
+    def __str__(self):
+        return self.title
