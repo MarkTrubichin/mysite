fe66013596ed5689451911ab929c9a5e2e7f51ae
MarkTru <bambambam228282@gmail.com>
2025-07-02 02:22:38 +0300

    initialized some pages and getting ready to work with db

---
+++ mysite/main/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+
+def index(request):
+    return render(request, 'main/index.html')
+
+def about(request):
+    return render(request, 'main/about.html')
+
+
+def home(request):
+    return render(request, 'main/home.html')
+
+def projects(request):
+    return render(request, 'main/projects.html')
